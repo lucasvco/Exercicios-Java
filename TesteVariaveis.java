00c6cc60c5fbb395e203972bb40607ab7eeac6f2
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2021-12-22 12:48:35 -0300

    Add files via upload

---
+++ TesteVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TesteVariaveis {
+	public static void main(String[] args) {
+		String nome = "lucas";
+		int idade = 29;
+		double altura = 1.70;
+		double salario = 3550.70;
+		System.out.println(salario);
+		System.out.println("Meu salario é " + (int) salario);
+		System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e minha altura é " + altura);
+	}
+}
