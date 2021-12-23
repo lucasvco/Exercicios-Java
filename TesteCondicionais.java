a28c8525854f6b5be16a0bd92ac06b92f64ff8ed
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2021-12-23 14:11:09 -0300

    Add files via upload

---
+++ TesteCondicionais.java
@@ -0,0 +1,14 @@
+
+public class TesteCondicionais {
+	public static void main(String[] args) {
+		int numero = 15;
+		boolean acompanhantes = true;
+		if(numero >= 18 || acompanhantes) {
+			System.out.println("Você tem mais de 18 ou esta acompanhado, pode entrar!");
+		}else {
+				System.out.println("Você não tem mais de 18 anos e não esta acompanhado. Não podera entrar!");
+			}
+		}
+	}
+
+
