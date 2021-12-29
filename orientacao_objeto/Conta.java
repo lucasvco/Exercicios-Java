bd663099c0928133214e682dad52b7d07e845b99
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2021-12-29 15:25:59 -0300

    Update Conta.java

--- orientacao_objeto/Conta.java
+++ orientacao_objeto/Conta.java
@@ -7,6 +7,15 @@ public class Conta {
 	String titular;
 	
 	public void depositar(double valor) {
-		this.saldo = this.saldo + valor;		
+		this.saldo += valor;		
+	}
+	
+	public boolean sacar(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
