2947a0d0467e87f6f573f437a04d99fe6325775f
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2022-01-11 21:38:10 -0300

    Update criaConta.java

--- orientacao_objeto/criaConta.java
+++ orientacao_objeto/criaConta.java
@@ -1,33 +1,62 @@
-
 public class criaConta {
 	public static void main(String[] args) {
+		Cliente lucasV = new Cliente();
+		lucasV.setNome("Lucas V.");
+		lucasV.setCpf("111.111.111-11");
+		lucasV.setEndereco("Taguatinga Norte");
+		lucasV.setProfissao("Desenvolvedor");
+		
+		Cliente luizaV = new Cliente();
+		luizaV.setNome("Luiza V.");
+		luizaV.setCpf("222.222.222-22");
+		luizaV.setEndereco("Taguatinga Sul");
+		luizaV.setProfissao("Ambientalista");
+		
+		Cliente brunaV = new Cliente();
+		brunaV.setNome("Bruna V.");
+		brunaV.setCpf("333.333.333-33");
+		brunaV.setEndereco("Taguatinga Centro");
+		brunaV.setProfissao("Professora");
+		
+		
 		Conta primeiraConta = new Conta();
-		primeiraConta.agencia = 123456;
-		primeiraConta.cpf = 03612312310;
-		primeiraConta.numero = 155;
-		primeiraConta.saldo = 200;
-		primeiraConta.titular = "Lucas V.";
+		primeiraConta.setAgencia(123456);
+		primeiraConta.setNumero(155);
+		primeiraConta.setTitular(lucasV);
 		
 		Conta segundaConta = new Conta();
-		segundaConta.agencia = 223456;
-		segundaConta.cpf = 03612312311;
-		segundaConta.numero = 256;
-		segundaConta.saldo = 300;
-		segundaConta.titular = "Bruna V.";
+		segundaConta.setAgencia(223456);
+		segundaConta.setNumero(255);
+		segundaConta.setTitular(brunaV);
 		
 		Conta terceiraConta = new Conta();
-		terceiraConta.agencia = 323456;
-		terceiraConta.cpf = 03612312312;
-		terceiraConta.numero = 357;
-		terceiraConta.saldo = 400;
-		terceiraConta.titular = "Luiza V.";
+		terceiraConta.setAgencia(323456);
+		terceiraConta.setNumero(355);
+		terceiraConta.setTitular(luizaV);
 		
+
 		primeiraConta.depositar(900);
-		System.out.println(primeiraConta.saldo);
+		System.out.println(primeiraConta.getSaldo());
+		
+		segundaConta.depositar(400);
 		segundaConta.sacar(150);
-		System.out.println(segundaConta.saldo);
+		System.out.println(segundaConta.getSaldo());
+		
+		terceiraConta.depositar(1000);
 		terceiraConta.transferir(200, segundaConta);
-		System.out.println(segundaConta.saldo);
-		System.out.println(terceiraConta.saldo);
+		System.out.println(segundaConta.getSaldo());
+		
+		System.out.println(terceiraConta.getSaldo());
+		
+		System.out.println(terceiraConta.getTitular().getEndereco());
+		
+		System.out.println(primeiraConta.getAgencia());
+		System.out.println(primeiraConta.getNumero());
+		System.out.println(segundaConta.getAgencia());
+		System.out.println(segundaConta.getNumero());
+		System.out.println(terceiraConta.getAgencia());
+		System.out.println(terceiraConta.getNumero());
+		
+		System.out.println(primeiraConta.getTitular().getNome());
 	}
 }
