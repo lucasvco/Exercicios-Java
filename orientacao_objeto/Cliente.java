0aa4604416bb35c827a2c377c4d48c6697601d92
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2022-01-11 21:38:35 -0300

    Update Cliente.java

--- orientacao_objeto/Cliente.java
+++ orientacao_objeto/Cliente.java
@@ -1,7 +1,33 @@
-
 public class Cliente {
-	String cpf;
-	String endereco;
-	String profissao;
-	String nome;
+	private String cpf;
+	private String endereco;
+	private String profissao;
+	private String nome;
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
