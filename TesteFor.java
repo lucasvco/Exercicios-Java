d5f995af2c11310bb2f2897d04866f4d816fa0f8
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2022-01-17 22:54:44 -0300

    Add files via upload

---
+++ TesteFor.java
@@ -0,0 +1,12 @@
+
+public class TesteFor {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			for(int contador = 0; contador <= 10; contador++) {
+				System.out.print(i*contador);	
+			}
+			System.out.println(" ");
+		}
+		System.out.println();
+	}
+}
---
+++ TesteWhile.java
@@ -0,0 +1,12 @@
+
+public class TesteWhile {
+	public static void main(String[] args) {
+		int contador = 0;
+		int numero = 0;
+		while (contador <= 10) {
+			numero = numero + contador;
+			System.out.println(numero);
+			contador++;
+		}
+	}
+}
