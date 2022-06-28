31df4415622026c1987f377f2acf7911451a198c
Lucas Veras C. de Oliveira <91347385+lucasvco@users.noreply.github.com>
2022-06-28 12:38:59 -0300

    Add files via upload
    Função recebe uma string e devolve a mesma ao contrario.

---
+++ testeStringContraria.java
@@ -0,0 +1,28 @@
+package java_nelio_alves_udemy;
+
+import java.util.Scanner;
+
+public class testeStringContraria {
+
+	public static void main(String[] args) {
+		contrario("minha pirocona");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite a frase");
+		String frase = sc.nextLine();
+		for (int i = frase.length() -1; i >= 0; i--) {
+			System.out.print(frase.charAt(i));
+		}
+		sc.close();
+	}
+	
+	public static String contrario(String x) {
+		for (int i = x.length() -1; i >= 0; i--) {
+			System.out.print(x.charAt(i));			
+		}
+		System.out.println("");
+		return x;
+	}
+	
+
+}
